811377d641161d3d7889823ca70e605523ce7f73
Nandushangrila <nivedr06@gmail.com>
2025-08-24 12:38:51 -0500

    inital commit

---
+++ swap.py
@@ -0,0 +1,15 @@
+a = 1
+b = 2
+c = 3
+
+print("Before swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
+
+a, b, c = c, a, b
+
+print("After swapping:")
+print("a =", a)
+print("b =", b)
+print("c =", c)
